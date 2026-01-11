bf98c35e4a3a66baaae7120ae56f930bb8275c38
riley <rileyneave00@gmail.com>
2026-01-11 03:54:09 -0600

    pink morsel from all 3 csv files

--- pink_morsel_data.py
+++ pink_morsel_data.py
@@ -16,7 +16,13 @@ with open('data/daily_sales_data_1.csv') as csvfile:
 
 import pandas as pd
 
-df = pd.read_csv('data/daily_sales_data_1.csv')
+files = [
+    "data/daily_sales_data_0.csv",
+    "data/daily_sales_data_1.csv",
+    "data/daily_sales_data_2.csv"
+]
+
+df = pd.concat([pd.read_csv(f) for f in files])
 df = df[df['product'] == 'pink morsel']
 df['price'] = df['price'].str.replace('$', '').astype(float)
 df['sales'] = df['price'] * df['quantity']
