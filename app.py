6932504c4ede20ec4be98e754fa9f43f3d6f5057
riley <rileyneave00@gmail.com>
2026-01-12 01:09:25 -0600

    interactive line chart based on regions

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
 app = Dash()
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
 df = pd.read_csv("data/pink_morsel_data.csv")
 df['date'] = pd.to_datetime(df['date'])
 df_2021 = df[(df['date'].dt.year == 2021) & (df['date'].dt.month == 1)]
-df_daily = df_2021.groupby("date")["sales"].sum().reset_index()
 
-fig = px.line(df_daily, x="date", y="sales", title="Pink Morsel Sales")
-fig.update_xaxes(
-    tickangle=45,
-    dtick='D1'
-)
+region_colors = {
+    'north' : 'red',
+    'south' : 'green',
+    'east' : 'blue',
+    'west' : 'orange',
+    'all' : 'purple'
+}
+
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
@@ -27,10 +26,39 @@ app.layout = html.Div(children=[
     '''),
 
     dcc.Graph(
-        id='sales-graph',
-        figure=fig
-    )
-])
+        id='sales-graph'
+    ),
+    html.Div(
+            dcc.RadioItems(
+                 options=[{'label': html.Span("North", style={'color': region_colors['north']}), 'value': 'north'},
+                        {'label': html.Span("South", style={'color': region_colors['south']}), 'value': 'south'},
+                        {'label': html.Span("East", style={'color': region_colors['east']}), 'value': 'east'},
+                        {'label': html.Span("West", style={'color': region_colors['west']}), 'value': 'west'},
+                        {'label': html.Span("All Regions", style={'color': region_colors['all']}), 'value': 'all'}
+                          ],
+                value = 'all', id='radio_items', inline = True
+            )
+            ,style={'width': '48%', 'float': 'right', 'display': 'inline-block'}
+        ),
+    ])
+
+@app.callback(
+    Output('sales-graph', 'figure'),
+    Input('radio_items', 'value'),
+)
+
+def update_graph(selected_region):
+    color = region_colors[selected_region]
+    if selected_region == 'all':
+        df_daily = df_2021.groupby("date")["sales"].sum().reset_index()
+        fig = px.line(df_daily, x = "date", y = "sales", title="All Regions")
+    else:
+        df_region = df_2021[df_2021['region'] == selected_region]
+        df_daily = df_region.groupby("date")["sales"].sum().reset_index()
+        fig = px.line(df_daily, x="date", y="sales", title=f"{selected_region.capitalize()}")
+    fig.update_traces(line = dict(color = color, width = 3))
+    fig.update_xaxes(tickangle=45, dtick='D1')
+    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
